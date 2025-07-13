03b9af9aaae1cb365455b43ed26353e1cc1510db
Buife-C <buifemede@gmail.com>
2025-07-13 00:40:25 -0400

    Fixed LoggedInView

--- src/main/java/view/LoggedInView.java
+++ src/main/java/view/LoggedInView.java
@@ -101,8 +101,8 @@ public class LoggedInView extends JPanel implements PropertyChangeListener {
                 evt -> {
                     if (evt.getSource().equals(logOut)) {
                         // TODO: execute the logout use case through the Controller
-                        // 1. get the state out of the loggedInViewModel. It contains the username.
-                        // 2. Execute the logout Controller.
+                        final LoggedInState currentState = loggedInViewModel.getState();
+                        logoutController.execute(currentState.getUsername());
                     }
                 }
         );
@@ -139,5 +139,6 @@ public class LoggedInView extends JPanel implements PropertyChangeListener {
 
     public void setLogoutController(LogoutController logoutController) {
         // TODO: save the logout controller in the instance variable.
+        this.logoutController = logoutController;
     }
 }
